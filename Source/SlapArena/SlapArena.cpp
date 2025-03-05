37984697f00f6435afbc300ca3bf0cfe812ae92e
Just <201133625+wolfgangkatzensprung@users.noreply.github.com>
2025-03-05 21:28:36 +0100

    Unreal Project
    Full Unreal Project, excluding external assets (see readme for details)

---
+++ Source/SlapArena/SlapArena.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SlapArena.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SlapArena, "SlapArena" );
